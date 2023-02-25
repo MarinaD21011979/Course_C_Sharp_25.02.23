7bc3405d61a5ff8216a41277a26b59d306e7c312
Marina <m.a.dedova@gmail.com>
2023-02-25 23:37:39 +0300

    week

---
+++ lesson_1/3_0/Program.cs
@@ -0,0 +1,41 @@
+﻿string s_a = Console.ReadLine();
+
+if (s_a == "1")
+{
+   Console.WriteLine("monday");
+}
+
+else if (s_a == "2")
+{
+    Console.WriteLine("tuesday");
+}
+
+else if (s_a == "3")
+{
+    Console.WriteLine("wedensday");
+}
+
+else if (s_a == "4")
+{
+    Console.WriteLine("thirsday");
+}
+
+else if (s_a == "5")
+{
+    Console.WriteLine("friday");
+}
+
+else if (s_a == "6")
+{
+    Console.WriteLine("saturday");
+}
+
+else if (s_a == "7")
+{
+    Console.WriteLine("sunday");
+}
+
+else
+{
+    Console.WriteLine("error");
+}
