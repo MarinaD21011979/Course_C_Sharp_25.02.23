bc694bd35733fee07bb4007a9519553bfc1e3981
Marina <m.a.dedova@gmail.com>
2023-02-28 21:06:08 +0300

    task free

---
+++ lesson_1/HW_lesson1/task1_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+int a = int.Parse(Console.ReadLine()!);
+
+if ( a % 2 == 0) Console.WriteLine("yes!");
+else Console.WriteLine("NO");
+
