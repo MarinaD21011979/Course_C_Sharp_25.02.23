75252d0208da1354c0bc4a3682d2a27cf76d6473
Marina <m.a.dedova@gmail.com>
2023-02-28 13:52:26 +0300

    program main

--- lesson_2/2_1/Program.cs
+++ lesson_2/2_1/Program.cs
@@ -1,39 +1,45 @@
 ﻿
-int TakeNum(int num)
+internal class Program
 {
-    int result1 = num % 10;
-    int result2 = num / 10;
-    if (result1 > result2)
+    private static void Main(string[] args)
     {
-        return result1;
-    }
-    else
-    {
-        return result2;
-    }
-    Console.WriteLine(num);
-    return num % 10;
-}
+        int TakeNum(int num)
+        {
+            int result1 = num % 10;
+            int result2 = num / 10;
+            if (result1 > result2)
+            {
+                return result1;
+            }
+            else
+            {
+                return result2;
+            }
+            Console.WriteLine(num);
+            return num % 10;
+        }
 
-int itog = TakeNum(new Random().Next(10, 100));
-Console.WriteLine(itog);
+        int itog = TakeNum(new Random().Next(10, 100));
+        Console.WriteLine(itog);
+    }
+}
 
 // // 1. Напишите программу, которая выводит случайное число
 //    из отрезка [10, 99] и показывает наибольшую цифру числа.
 
 // int TakeNum(int num)
 // {
-    // Console.WriteLine(num);
-    // int resulut1 = num % 10;
-    // int resulut2 = num / 10;
-    // if (resulut1 > resulut2)
-    // {
-        // return resulut1;
-    // }
-    // else
-    // {
-        // return resulut2;
-    // }
+// Console.WriteLine(num);
+// int resulut1 = num % 10;
+// int resulut2 = num / 10;
+// if (resulut1 > resulut2)
+// {
+// return resulut1;
+// }
+// else
+// {
+// return resulut2;
+// }
 // }
 
 // int itog = TakeNum(new Random().Next(10, 100));
