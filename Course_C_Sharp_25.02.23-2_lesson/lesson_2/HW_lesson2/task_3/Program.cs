6a9f34f2f8c1bf53a6218655bd73a87aed56ce8f
Marina <m.a.dedova@gmail.com>
2023-03-04 21:49:50 +0300

    demo task

---
+++ Course_C_Sharp_25.02.23-2_lesson/lesson_2/HW_lesson2/task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+
+int num = int.Parse(Console.ReadLine()!);
+
+    if (num < 100) Console.WriteLine("no third digit");
+    else
+    {
+        while (num > 999) Console.WriteLine(num / 10);     
+    }
+    Console.WriteLine(num % 10);  
+
--- lesson_3/task3_0/Program.cs
+++ lesson_3/task3_0/Program.cs
@@ -1,9 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// 1. Напишите программу, которая принимает на вход координаты точки (X и Y),
+//    причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости,
+//    в которой находится эта точка.
 
-void Quarters(int x, int y)
+void Quarters(int x, int y) // void - функция, которая больше информирует
 {
     if (x == 0 || y == 0)
-    Console.WriteLine("x = 0, y = 0");
-    
+        Console.WriteLine("x = 0, y = 0");
+    if (x > 0 && y > 0)
+        Console.WriteLine("One");
+    if (x < 0 && y > 0)
+        Console.WriteLine("Two");
+    if (x < 0 && y < 0)
+        Console.WriteLine("Three");
+    if (x > 0 && y < 0)
+        Console.WriteLine("Four"); 
 }
-Console.WriteLine("Hello, World!");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Quarters(num1 , num2);
