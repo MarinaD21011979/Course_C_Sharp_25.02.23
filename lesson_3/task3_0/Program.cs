d180f4ea334b91fd1f5f6fb6cc50c71dbf3b1264
Marina <m.a.dedova@gmail.com>
2023-03-04 10:33:07 +0300

    task two

---
+++ lesson_3/task3_0/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+    Console.WriteLine("x = 0, y = 0");
+    
+}
+Console.WriteLine("Hello, World!");
---
+++ lesson_3/task3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+void Quarters(int num)
+{
+    if (num == 1)
+        Console.WriteLine("x > 0, y > 0");
+    else if (num == 2)
+        Console.WriteLine("x < 0 && y > 0");
+    else if (num == 3)
+        Console.WriteLine("x < 0 && y < 0");
+    else if (num == 4)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+}
+
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Quarters(num_1, num_2);
+
