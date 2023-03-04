37401e62c91cc044b04d0645a58ac38607a09d22
Marina <m.a.dedova@gmail.com>
2023-03-04 22:45:27 +0300

    method for

--- lesson_3/task3_3/Program.cs
+++ lesson_3/task3_3/Program.cs
@@ -1,4 +1,15 @@
 ﻿// 3. Напишите программу, которая принимает на вход число (N) 
 //    и выдаёт таблицу квадратов чисел от 1 до N.
+// цикл for
 
+void Kvadrats(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        Console.Write(i * i + ",");
+    }
+
+}
+int x = int.Parse(Console.ReadLine()!);
+Kvadrats(x);
 
