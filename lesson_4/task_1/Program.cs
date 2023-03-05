768fd0d3364051f99cd1b99f89d9f01e0b1593d7
Marina <m.a.dedova@gmail.com>
2023-03-05 10:57:02 +0300

    lesson four

---
+++ lesson_4/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+int NumDig(int num)
+{
+    int dig1 = 0;
+    for (int i = 0; num > 0; i++){
+        num = num / 10;
+        dig1 = 1;
+    }
+    return dig1;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumDig(A));
