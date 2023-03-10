c1eae76a8ea9a9c3256f6996320f704b24a2651b
Marina <m.a.dedova@gmail.com>
2023-03-10 21:39:25 +0300

    cube with for

---
+++ lesson_3/HW_lesson_3/task3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// WITH for
+
+void Cube(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine(i * i * i +",");
+    }
+}
+int x = int.Parse(Console.ReadLine()!);
+Cube(x);
