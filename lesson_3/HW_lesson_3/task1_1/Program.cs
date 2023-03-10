80a680d2315f16a13ee9a9b323ea24451e889868
Marina <m.a.dedova@gmail.com>
2023-03-10 19:34:37 +0300

    without variable

---
+++ lesson_3/HW_lesson_3/task1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// БЕЗ переменных
+
+void FindNum(int num)
+{
+    Console.WriteLine(num);
+
+    if ( num / 10000 == num % 10 && num / 1000 == num % 100 )
+    {
+        Console.WriteLine("palindrom");
+    }
+    
+    else
+    {
+        Console.WriteLine("NO");
+    } 
+    
+}
+FindNum(int.Parse(Console.ReadLine()));
+FindNum(new Random().Next(10000, 99999));
