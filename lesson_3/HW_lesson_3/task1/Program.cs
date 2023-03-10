2bf5d80a3fc32d1f93b7b260b5b2de4b6295ea40
Marina <m.a.dedova@gmail.com>
2023-03-10 19:13:12 +0300

    the first task

---
+++ lesson_3/HW_lesson_3/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void FindNum(int num)
+{
+    int digitFirst = num / 10000;
+    int digitLast = num % 10;
+    int digitSecond = num / 1000;
+    int digitPreLast = num % 100;
+    if (digitFirst == digitLast && digitSecond == digitPreLast)
+    {
+        Console.WriteLine("Palindrom!");
+    } 
+    else
+    {
+        Console.WriteLine("NO");
+    }
+    
+}
+
+FindNum (int.Parse(Console.ReadLine()));
+FindNum(new Random().Next(10000, 99999));
+
+
