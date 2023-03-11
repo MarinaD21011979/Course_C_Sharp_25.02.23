5cba14c856b4c2dbc1b42ee149c98c58229696b0
Marina <m.a.dedova@gmail.com>
2023-03-11 15:51:16 +0300

    task two with for

---
+++ lesson_4/HW_lesson4/task2_for/Program.cs
@@ -0,0 +1,13 @@
+﻿// на вход число и поиск суммы цифр в этом числе с помощью for
+
+int SumDigs(int num)
+{
+    int dig1 = 0;
+
+    for (int i = num; num != 0; num /=10) // ! НЕ равно, то есть пока число не равно нулю
+        dig1 += num % 10;
+
+        return dig1;
+}
+int dig1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumDigs(dig1));
