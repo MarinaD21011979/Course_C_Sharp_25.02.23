4c39f09d9f810ab3aabfa9d842bdda22779db308
Marina <m.a.dedova@gmail.com>
2023-03-11 19:47:09 +0300

    troubleshouting

--- lesson_5/task1/Program.cs
+++ lesson_5/task1/Program.cs
@@ -2,42 +2,42 @@
 // положительные элементы замените на соответсвующие орицательные  и наоборот
 
 void Print(int[] arr)
-	{
-	    int size = arr.Length;
+{
+	int size = arr.Length;
 	
-	    for (int i = 0; i < size; i++)
-	        Console.Write($"{arr[i]} ");
-	    Console.WriteLine();
-	}
+	for (int i = 0; i < size; i++)
+	    Console.Write($"{arr[i]} ");
+	Console.WriteLine();
+}
 	
-	int[] MassNums(int size, int from, int to)
-	{
-	    int[] arr = new int[size];
+int[] MassNums(int size, int from, int to)
+{
+	int[] arr = new int[size];
 	
-	    for (int i = 0; i < size; i++)
-	        arr[i] = new Random().Next(from, to);
-	    return arr;
-	}
+	for (int i = 0; i < size; i++)
+	    arr[i] = new Random().Next(from, to);
+	return arr;
+}
 	
-	void SumPosNeg(int[] arr)
-	{
+void SumPosNeg(int[] arr)
+{
 	    
-	    for (int i = 0; i < arr.Length; i++)
-	    {
-	        arr[i] = -arr[i];
+	for (int i = 0; i < arr.Length; i++)
+	{
+	    arr[i] = -arr[i];
                
-	    }
-        
 	}
+        
+}
 	
 	
-	int num = int.Parse(Console.ReadLine()!);
-	int start = int.Parse(Console.ReadLine()!);
-	int stop = int.Parse(Console.ReadLine()!);
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
 	
-	int[] mass = MassNums(num, start, stop);
-	Print(mass);
-	SumPosNeg(mass);
-	Print(mass);
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+SumPosNeg(mass);
+Print(mass);
 
 
