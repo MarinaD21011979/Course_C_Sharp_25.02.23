cf7c1e446544df3f7f6b7303ac6507a9e646878d
Marina <m.a.dedova@gmail.com>
2023-03-11 15:26:50 +0300

    task one

--- lesson_4/HW_lesson4/task1/Program.cs
+++ lesson_4/HW_lesson4/task1/Program.cs
@@ -3,4 +3,26 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+double PowNum(int a, int b)
+{
+    double n_pow = 1; // 1 потмоу что произведение
+    int b_abs = Math.Abs(b);
+
+    for (int i = 1; i <= b_abs; i++) // i <= количество итераций
+    {
+        if (b > 0)
+            n_pow *= a;
+        else
+            n_pow /= a;  // если  степень отрицательная 
+    }
+    return n_pow;
+}
+
+Console.WriteLine("Enter a number: ");
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter degree of number; ");
+int num_2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PowNum(num_1, num_2));
+
 
