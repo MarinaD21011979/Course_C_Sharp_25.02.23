7a46241c3ebbdbb059bdb47c0ce05a3034cef1d8
Marina <m.a.dedova@gmail.com>
2023-03-12 11:01:50 +0300

    task two

---
+++ lesson_6/task6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая будет преобразовывать десятичное число в двоичное
+// доп строковая переменная - позволяет сразу получить результат вычислений
+// в стринг преобразовывать не надо
+
+string Convert (int num)
+{
+    string result = ""; // empty string & result variable
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2; // уменьшаем на 2 или =/ 2
+    }
+    return result;
+}
+
+Console.WriteLine(Convert(12)); // int.Parse, string result = F, 
