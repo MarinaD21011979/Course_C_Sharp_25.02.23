86b13c747095513d0fff707125a77bb57070a621
Marina <m.a.dedova@gmail.com>
2023-03-17 22:58:44 +0300

    hw task two

---
+++ lesson_5/HW5/5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int [size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void SumNumNotChet(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i +=2);
+
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+SumNumNotChet(mass);
