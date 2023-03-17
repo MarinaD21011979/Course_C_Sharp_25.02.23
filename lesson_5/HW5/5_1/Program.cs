d8d5842c06c8cc09f37bf8507737156d9ba868d7
Marina <m.a.dedova@gmail.com>
2023-03-17 21:44:28 +0300

    hw task one

---
+++ lesson_5/HW5/5_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void Print(int[] arr)
+{
+	int size = arr.Length;
+	
+	for (int i = 0; i < size; i++)
+	    Console.Write($"{arr[i]} ");
+	Console.WriteLine();
+}
+	
+int[] MassNums(int size, int from, int to)
+{
+	int[] arr = new int[size];
+	
+	for (int i = 0; i < size; i++)
+	    arr[i] = new Random().Next(from, to);
+	return arr;
+}
+
+int KolChetNum(int[] arr)
+{
+    int chet = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 100 && arr[i] <= 999)
+        if (chet % 2 == 0)
+        chet ++;  
+    }
+    return chet;
+    
+}	
+
+			
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[] mass = MassNums(num, start, stop); // сборка массива: кол-во цифр, начало и конец
+Print(mass);
+Console.WriteLine(KolChetNum(mass));
+
+	
