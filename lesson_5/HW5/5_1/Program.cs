85fcad4161f8d04a985654054b1ea0a684a128be
Marina <m.a.dedova@gmail.com>
2023-03-17 23:26:13 +0300

    correction

--- lesson_5/HW5/5_1/Program.cs
+++ lesson_5/HW5/5_1/Program.cs
@@ -6,7 +6,6 @@
 void Print(int[] arr)
 {
 	int size = arr.Length;
-	
 	for (int i = 0; i < size; i++)
 	    Console.Write($"{arr[i]} ");
 	Console.WriteLine();
@@ -15,7 +14,6 @@ void Print(int[] arr)
 int[] MassNums(int size, int from, int to)
 {
 	int[] arr = new int[size];
-	
 	for (int i = 0; i < size; i++)
 	    arr[i] = new Random().Next(from, to);
 	return arr;
@@ -27,15 +25,14 @@ int KolChetNum(int[] arr)
     for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i] >= 100 && arr[i] <= 999)
+        Console.WriteLine(chet);
         if (chet % 2 == 0)
-        chet ++;  
+        chet++; 
     }
-    return chet;
-    
+    return chet;   
 }	
 
-			
-int num = int.Parse(Console.ReadLine()!);
+int num = int.Parse(Console.ReadLine()!); 			
 int start = int.Parse(Console.ReadLine()!);
 int stop = int.Parse(Console.ReadLine()!);
 int[] mass = MassNums(num, start, stop); // сборка массива: кол-во цифр, начало и конец
