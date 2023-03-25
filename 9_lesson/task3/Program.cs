ac8869297120c204c00c8f6b8abb1da33152167f
Marina <m.a.dedova@gmail.com>
2023-03-25 10:53:22 +0300

    task3

---
+++ 9_lesson/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// // 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+int ValueSet(int M)
+{
+    if (M == 0) return 0;
+    return ValueSet(M / 10) + M % 10;
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ValueSet(num1));
