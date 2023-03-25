aeb410524c3843f3377387f341f879404aaff4ca
Marina <m.a.dedova@gmail.com>
2023-03-25 11:02:04 +0300

    task4

---
+++ 9_lesson/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// // 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+int ValueSet(int A, int B)
+{
+    if (B == 0) return 1;
+    return ValueSet(A, B -1) *A;  
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+ValueSet(num1, num2);
