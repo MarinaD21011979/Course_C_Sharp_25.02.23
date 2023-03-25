03f04c2ed581346338c48567a36edfc00958625c
Marina <m.a.dedova@gmail.com>
2023-03-25 10:41:40 +0300

    task2

---
+++ 9_lesson/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// / 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void ValueSet(int M, int N)
+{
+    if (N == M) return;
+    ValueSet(M, N - 1);
+    Console.Write($"{N} ");
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+ValueSet(num1, num2);
